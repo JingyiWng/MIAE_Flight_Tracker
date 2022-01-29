855d289216e1684478eb39b7579e88fb39eeffb1
Jingyi Wang <jingyi.wang2@mail.mcgill.ca>
2022-01-29 11:04:41 -0500

    Commit main file

---
+++ main.py
@@ -0,0 +1,14 @@
+def main_wrapper():
+    print(f"This is the start of the the Python project. The name of this function is {main_wrapper.__name__}") #Even the function we defined has an attribute
+
+    # Code here
+    #
+
+
+
+
+    print("This is the end of the the Python project")
+    pass
+
+if __name__ == "__main__":
+    main_wrapper()
