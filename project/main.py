9270bac61d90720ad3f1c6ed83ff8fe95d157d8e
Jingyi Wang <jingyi.wang2@mail.mcgill.ca>
2022-01-29 11:28:18 -0500

    BREAKING CHANGE

--- project/main.py
+++ project/main.py
@@ -10,6 +10,7 @@ def main_wrapper():
     # project structuring for modularity, maintainability, and separation of concerns.
     # 2. git pull
 
+    THIS IS A BREAKING CHANGE
     print("This is the end of our python project")
 
 
